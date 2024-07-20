5d35d59f64edd8ceb2d631810d3939e0ba4e926a
JLR - Fahazavana <rjlucienaina@gmail.com>
2024-07-20 09:42:05 +0200

    Fix loss log

--- src/Normalizer.py
+++ src/Normalizer.py
@@ -2,38 +2,40 @@ import re
 
 
 def process_line(line):
-	line = re.sub(r'(\.$|!$|\?$)', ' <eos> ', line)
-	line = re.sub(r"('[a-zA-Z]{1})\b", r" \1", line)
-	line = re.sub(r"(:|,|``|'')", r" \1 ", line)
+    line = re.sub(r"(\.$|!$|\?$)", r" \1", line)
+    line = re.sub(r"(\.$|!$|\?$)", r"<eos>", line)
+    line = re.sub(r"('[a-zA-Z]{1})\b", r" \1", line)
+    line = re.sub(r"(:|,|``|'')", r" \1 ", line)
 
-	line = re.sub(r'\s{2,}', ' ', line)
+    line = re.sub(r"\s{2,}", " ", line)
 
-	pattern = r'\$([^$]+)\$'
-	new_text = ""
-	last_end = 0
+    pattern = r"\$([^$]+)\$"
+    new_text = ""
+    last_end = 0
 
-	for match in re.finditer(pattern, line):
-		start, end = match.span()
-		new_text += line[last_end:start] + " $" + match.group(1).replace(" ", "") + "$ "
-		last_end = end
+    for match in re.finditer(pattern, line):
+        start, end = match.span()
+        new_text += line[last_end:start] + " $" + match.group(1).replace(" ", "") + "$ "
+        last_end = end
 
-	new_text += line[last_end:]
-	text = new_text
-	text = re.sub(r"\s{2,}", " ", text)
-	text = text.lower()
-	text = text.split()
-	if text[-1] != "<eos>":
-		text.append("<eos>")
-	return ["<sos> "] + text
+    new_text += line[last_end:]
+    text = new_text
+    text = re.sub(r"\s{2,}", " ", text)
+    text = text.lower()
+    line = re.sub(r"\s{2,}", " ", line)  # ???? But it works
+    text = text.strip().split()
+    if text[-1] != "<eos>":
+        text.append("<eos>")
+    return ["<sos>"] + text
 
 
 def preprocess_data(train_raw_dir, train_data_dir, language_files, language_name):
-	with open(f"{train_data_dir}/{language_name}.txt", 'w+') as output_file:
-		for file_name in language_files:
-			with open(f"{train_raw_dir}/{file_name}") as input_file:
-				for line in input_file:
-					line = line.strip()
-					if len(line) > 0 and line[0] != "%":
-						processed_line = process_line(line)
-						output_file.write(" ".join(processed_line) + "\n")
-	print(f"Done for {language_name}!")
+    with open(f"{train_data_dir}/{language_name}.txt", "w+") as output_file:
+        for file_name in language_files:
+            with open(f"{train_raw_dir}/{file_name}") as input_file:
+                for line in input_file:
+                    line = line.strip()
+                    if len(line) > 0 and line[0] != "%":
+                        processed_line = process_line(line)
+                        output_file.write(" ".join(processed_line) + "\n")
+    print(f"Done for {language_name}!")
--- src/utils.py
+++ src/utils.py
@@ -10,7 +10,7 @@ from tqdm import tqdm
 
 
 def get_device():
-    if platform.platform().lower().startswith('mac'):  # macOS
+    if platform.platform().lower().startswith("mac"):  # macOS
         return "mps" if torch.backends.mps.is_available() else "cpu"
     else:  # Linux, Windows
         return "cuda" if torch.cuda.is_available() else "cpu"
@@ -33,13 +33,32 @@ def load_config(config_file="config.json"):
     with open(config_file, "r") as f:
         data = json.load(f)
 
-    return Config(DATA_BASE=data["DATA_BASE"], TRAIN_RAW=data["TRAIN_RAW"], TRAIN_DATA=data["TRAIN_DATA"],
-        VAL_RAW=data["VAL_RAW"], VAL_DATA=data["VAL_DATA"], TRAIN_SOURCE=data["TRAIN_SOURCE"],
-        VAL_SOURCE=data["VAL_SOURCE"], TRAIN_TARGET=data["TRAIN_TARGET"], VAL_TARGET=data["VAL_TARGET"], )
+    return Config(
+        DATA_BASE=data["DATA_BASE"],
+        TRAIN_RAW=data["TRAIN_RAW"],
+        TRAIN_DATA=data["TRAIN_DATA"],
+        VAL_RAW=data["VAL_RAW"],
+        VAL_DATA=data["VAL_DATA"],
+        TRAIN_SOURCE=data["TRAIN_SOURCE"],
+        VAL_SOURCE=data["VAL_SOURCE"],
+        TRAIN_TARGET=data["TRAIN_TARGET"],
+        VAL_TARGET=data["VAL_TARGET"],
+    )
 
 
-def train_model(model, train_loader, optimizer, criterion, device, epochs, source_test, reference, translator):
+def train_model(
+    model,
+    train_loader,
+    optimizer,
+    criterion,
+    device,
+    epochs,
+    source_test,
+    reference,
+    translator,
+):
     metric = evaluate.load("bleu", cache_dir="./")
+    train_loss = []
     N = len(train_loader.dataset)
     for epoch in range(epochs):
         pbar = tqdm(train_loader, unit="batch", desc=f"Epoch {epoch + 1}/{epochs}")
@@ -58,18 +77,26 @@ def train_model(model, train_loader, optimizer, criterion, device, epochs, sourc
 
             torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1)
             optimizer.step()
-            run_loss += loss.item() * source.size(0)
-            pbar.set_postfix(loss=f"{run_loss / N:.3f}")
-
+            run_loss += loss.item() * source.size(0) 
+            pbar.set_postfix(loss=f"{run_loss/N:.3f}")
+        train_loss.append(run_loss / N)
         predicted = translator.translate_sentence(source_test)
         bleu = sentence_bleu([predicted], [reference], metric)
         print(f"Predicted: {predicted}")
         print(f"BLEU Score: {bleu}")
+    return train_loss
 
 
 def sentence_bleu(prediction, reference, metric=evaluate.load("bleu", cache_dir="./")):
-    blues = [np.round(metric.compute(predictions=prediction, references=reference, max_order=i)['bleu'], 3) for i in
-             range(1, 5)]
+    blues = [
+        np.round(
+            metric.compute(predictions=prediction, references=reference, max_order=i)[
+                "bleu"
+            ],
+            3,
+        )
+        for i in range(1, 5)
+    ]
     return blues
 
 
@@ -78,7 +105,9 @@ def corpus_bleu(predictions, references):
     for i in range(1, 5):
         print(f"BLEU{-i}".center(80))
         print("-----" * 18)
-        blues = metric.compute(predictions=predictions, references=references, max_order=i)
+        blues = metric.compute(
+            predictions=predictions, references=references, max_order=i
+        )
         for key, val in blues.items():
             print(f"{key:<20}: {val}")
         print("*****" * 18)
